cc0d9dd714c2d497424c7272c3ea6a49a10332d3
pawpiotro <pawpiotro22@gmail.com>
2019-05-29 20:19:27 +0200

    Lemmatization fixed

--- lemma_tokenizer.py
+++ lemma_tokenizer.py
@@ -1,6 +1,16 @@
-from nltk import word_tokenize
+from nltk import word_tokenize, pos_tag
 from nltk.stem import WordNetLemmatizer
-from nltk.corpus import stopwords
+from nltk.corpus import stopwords, wordnet
+
+
+def get_wordnet_pos(word):
+    tag = pos_tag([word])[0][1][0].upper()
+    tag_dict = {"J": wordnet.ADJ,
+                "N": wordnet.NOUN,
+                "V": wordnet.VERB,
+                "R": wordnet.ADV}
+
+    return tag_dict.get(tag, wordnet.NOUN)
 
 
 class LemmaTokenizer(object):
@@ -8,7 +18,7 @@ class LemmaTokenizer(object):
         self.wnl = WordNetLemmatizer()
 
     def __call__(self, doc):
-        return [self.wnl.lemmatize(t)
+        return [self.wnl.lemmatize(t, get_wordnet_pos(t))
                 for t in word_tokenize(doc)]
 
 
@@ -18,9 +28,9 @@ class LemmaTokenizer2(object):
         self.wnl = WordNetLemmatizer()
 
     def __call__(self, doc):
-        return [self.wnl.lemmatize(t)
+        return [self.wnl.lemmatize(t, get_wordnet_pos(t))
                 for t in word_tokenize(doc)
                 if t not in stopwords.words('english')]
 
 
-lemma_stopwords = [WordNetLemmatizer().lemmatize(t) for t in stopwords.words('english')]
+lemma_stopwords = [WordNetLemmatizer().lemmatize(t, get_wordnet_pos(t)) for t in stopwords.words('english')]
